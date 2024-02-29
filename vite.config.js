b786378346e6ebd13402bb125d6d9cf098448ce9
drkhaikDev <trinhkhaiviet@gmail.com>
2024-02-29 17:53:37 +0700

    fix vite config to deploy on render

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    strictPort: true,
     port: 3000,
   },
   // https://github.com/vitejs/vite/issues/12246#issuecomment-1450153095
